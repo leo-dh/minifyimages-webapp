73b9777ee0eeae9ec58ab1cc0109d7029f4b8b76
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-01-09 21:07:04 +0800

    added workerize loader

--- minifyimages-preact/gatsby-config.js
+++ minifyimages-preact/gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
     `gatsby-plugin-typescript`,
     `gatsby-plugin-preact`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-workerize-loader`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
