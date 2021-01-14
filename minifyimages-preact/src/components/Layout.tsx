3044801bc46316370671e80e50e1e8f35337e8c3
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-01-14 19:13:08 +0800

    added simple nav

--- minifyimages-preact/src/components/Layout.tsx
+++ minifyimages-preact/src/components/Layout.tsx
@@ -1,4 +1,17 @@
-import { h, ComponentChildren } from 'preact';
+import { h, Fragment, ComponentChildren } from 'preact';
 export default function Layout({ children }: { children: ComponentChildren }) {
-  return <div className="mx-auto container">{children}</div>;
+  return (
+    <>
+      <nav className="bg-purple-500 w-full">
+        <div className="mx-auto container px-4 sm:px-8 py-2 max-w-screen-xl">
+          <h2 className="text-white font-extrabold text-2xl pointer-events-none select-none">
+            Minify Images
+          </h2>
+        </div>
+      </nav>
+      <div className="mx-auto container p-4 sm:p-8 max-w-screen-xl">
+        {children}
+      </div>
+    </>
+  );
 }
